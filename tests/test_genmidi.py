ede99e832eb0a2388bebf84d114384e34c9a8130
Pedro Kroger <kroger@pedrokroger.net>
2012-03-31 18:47:04 -0300

    Test rests

--- tests/test_genmidi.py
+++ tests/test_genmidi.py
@@ -6,7 +6,7 @@ from pyknon.musiclib import NoteSeq, Note
 
 class TestRest(unittest.TestCase):
     def test_write_midifile(self):
-        notes1 = NoteSeq("D4 F#8 A")
+        notes1 = NoteSeq("D4 F#8 R A")
         midi = Midi(1, tempo=133)
         midi.seq_notes(notes1, track=0)
         midi.write_file(tempfile.TemporaryFile())
