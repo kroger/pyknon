936de0d0944d675e9a27ae0a32cad0e335e3cc8d
Pedro Kroger <kroger@pedrokroger.net>
2012-07-26 11:23:08 -0300

    Update sample to use new code. Fixes #1

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 from pyknon.genmidi import Midi
-from pyknon.musiclib import NoteSeq
+from pyknon.music import NoteSeq
 
 notes1 = NoteSeq("D4 F#8 A Bb4")
 midi = Midi(1, tempo=90)
 midi.seq_notes(notes1, track=0)
-midi.write_file("demo.mid")
+midi.write("demo.mid")
