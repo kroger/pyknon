6aaec2fbbde95b36bd8debf62c1bbb40d7f78085
Pedro Kroger <kroger@pedrokroger.net>
2012-07-27 00:30:21 -0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyknon',
-    version='0.1',
+    version='1.0',
     packages=['pyknon',],
     license='MIT',
     long_description=open('README').read(),
