bfc347d0c1ae6023c60f865212d77421bd2ccea3
Pedro Kroger <kroger@pedrokroger.net>
2012-07-30 14:17:10 -0300

    Add url, author, and email metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
 setup(
     name='pyknon',
     version='1.0',
     packages=['pyknon',],
+    url="http://kroger.github.com/pyknon",
     license='MIT',
+    author='Pedro Kroger',
+    author_email='kroger@pedrokroger.net',
     long_description=open('README').read(),
 )
